50da0a031acd97c74b6ced197aef50e67dad8238
倪程伟 <nichengwei120@163.com>
2016-06-17 15:33:09 +0800

    Update 0017.py

--- 0017.py
+++ 0017.py
@@ -6,15 +6,26 @@
 from lxml import etree
 import xlrd, codecs, pickle
 
-if __name__ == '__main__':
-    excel = xlrd.open_workbook('student.xls')
-    student = excel.sheet_by_name('student')
+def load_excel(file_name, sheet_name):
+    excel = xlrd.open_workbook(file_name)
+    student = excel.sheet_by_name(sheet_name)
     data = {}
     for row in range(student.nrows):
         data[student.row_values(row)[0]] = student.row_values(row)[1:]
-    root = etree.Element('root')
-    students = etree.SubElement(root, 'students')
-    students.append(etree.Comment("学生信息表\"id\" : [名字, 数学, 语文, 英文]"))
-    students.text = str(data)
+    return data
+
+def create_xml(file_name, ele, sub_ele, cmt, info):
+    root = etree.Element(ele)
+    students = etree.SubElement(root, sub_ele)
+    students.append(etree.Comment(cmt))
+    students.text = str(info)
     student_xml = etree.ElementTree(root)
-    student_xml.write('student.xml', pretty_print=True, xml_declaration=True, encoding='utf-8')
+    student_xml.write(file_name, pretty_print=True, xml_declaration=True, encoding='utf-8')
+
+if __name__ == '__main__':
+    file_name = 'student.xls'
+    sheet_name = 'student'
+    comment = '学生信息表"id" : [名字, 数学, 语文, 英文]'
+
+    student_info = load_excel(file_name, sheet_name)
+    create_xml('student.xml', 'root', sheet_name, comment, student_info)
